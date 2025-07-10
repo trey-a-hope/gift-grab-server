c6e9591ff85263474ca7789f2b45951d47bf16cb
Trey Hope <trey.a.hope@gmail.com>
2025-07-10 11:59:27 -0500

    Remove unused modules and handlers
    * Deleted account and leaderboard modules along with their associated functions.
    * Removed the module initialization logic from main.go.
    * Cleaned up the notifications handler as it is no longer needed.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"database/sql"
-	"gift-grab-server/internal/module"
 	"time"
 
 	"github.com/heroiclabs/nakama-common/runtime"
@@ -13,11 +12,6 @@ func main() {}
 
 func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error {
 	initStart := time.Now()
-
-	if err := module.Initialize(ctx, logger, db, nk, initializer); err != nil {
-		logger.Error("Failed to initialize module: %v", err)
-		return err
-	}
 
 	logger.Info("Module loaded in %dms", time.Since(initStart).Milliseconds())
 	return nil
