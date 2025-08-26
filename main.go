a66d5b52eed42824ffeac2fda80b83c55bc98f6c
Trey Hope <trey.a.hope@gmail.com>
2025-08-26 13:25:37 -0400

    Add account deletion RPC handler and register it in main module
    * Introduced a new account package with DeleteId function for account deletion.
    * Updated main.go to register the account_delete_id RPC with the initializer.
    * Enhanced error handling for account deletion in the new handler.

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"database/sql"
+	"gift-grab-server/controllers/account"
 	"time"
+
 	"github.com/heroiclabs/nakama-common/runtime"
 )
 
@@ -12,6 +14,19 @@ func main() {}
 func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error {
 	initStart := time.Now()
 
+	if err := registerRPCS(initializer, logger); err != nil {
+		return err
+	}
+
 	logger.Info("Module loaded in %dms", time.Since(initStart).Milliseconds())
+	return nil
+}
+
+func registerRPCS(initializer runtime.Initializer, logger runtime.Logger) error {
+	if err := initializer.RegisterRpc("account_delete_id", account.DeleteId); err != nil {
+		logger.Error("Unable to register: %v", err)
+		return err
+	}
+
 	return nil
 }
